8e1f93129093441c8d615d1ecd96db2af50adc8e
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-06 09:08:33 +0200

    async joke

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const button = document.getElementById('jokeBtn');
 
 button.addEventListener('click', generateJoke);
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   };
+  const res = await fetch('https://icanhazdadjoke.com/', config);
 
-  fetch('https://icanhazdadjoke.com/', config)
+  const data = await res.json();
+
+  jokeElement.innerHTML = data.joke;
+
+  /*   fetch('https://icanhazdadjoke.com/', config)
     .then((res) => res.json())
     .then((data) => {
       jokeElement.innerHTML = data.joke;
-    });
+    }); */
 }
